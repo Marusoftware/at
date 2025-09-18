134bcc9ad8214cb87806954d421c8bf9fb8c6dfd
Maruo.S <raspi-maru2004@outlook.jp>
2025-09-18 12:22:34 +0900

    change endpoint

--- server/at/routes/message.py
+++ server/at/routes/message.py
@@ -11,7 +11,7 @@ from .auth import get_user_no_error, get_user
 
 router=APIRouter(tags=["Message"])
 
-@router.get("/", response_model=List[Thread])
+@router.get("/thread/", response_model=List[Thread])
 async def get_threads(user:Union[UserDB,None] = Depends(get_user_no_error)):
     if user is None:
         return await ThreadDB.filter(owner=None)
@@ -19,7 +19,7 @@ async def get_threads(user:Union[UserDB,None] = Depends(get_user_no_error)):
         return await ThreadDB.filter(owner=None)
     return await ThreadDB.filter(Q(owner=None) | Q(owner=user))
 
-@router.get("/{thread_id}/", response_model=Thread)
+@router.get("/thread/{thread_id}/", response_model=Thread)
 async def get_thread(thread_id:UUID, user:Union[UserDB,None] = Depends(get_user_no_error)):
     if user is None:
         thread=await ThreadDB.get_or_none(owner=None, id=thread_id)
@@ -32,7 +32,7 @@ async def get_thread(thread_id:UUID, user:Union[UserDB,None] = Depends(get_user_
         raise NotFound(detail="Thread not found")
     return thread
 
-@router.post("/", response_model=Thread)
+@router.post("/thread/", response_model=Thread)
 async def create_thread(thread:ThreadCreate, user:UserDB = Depends(get_user)):
     thread_db = await ThreadDB.create(**thread.model_dump(), owner=user)
     return thread_db
